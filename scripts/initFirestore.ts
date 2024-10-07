f5475869431d11927b02c45a0cebf1999faf9b93
maiqel <michaelolowe321@gmail.com>
2024-10-07 14:19:32 +0100

    fix: fixed init fireStore issues

--- scripts/initFirestore.ts
+++ scripts/initFirestore.ts
@@ -1,8 +1,9 @@
-import "dotenv/config";
 import { initializeApp } from "firebase/app";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
+import * as dotenv from "dotenv";
 
-// Firebase configuration
+dotenv.config({ path: ".env.local" });
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -10,9 +11,9 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 

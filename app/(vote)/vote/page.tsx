133975e55849239c49e130f4cde022f18e61f18d
Olowe michael <66220194+Maiqel1@users.noreply.github.com>
2024-10-11 20:06:57 +0100

    chorw]

--- app/(vote)/vote/page.tsx
+++ app/(vote)/vote/page.tsx
@@ -66,7 +66,7 @@ export default function VotingPage() {
       }
 
       const voterData = voterDoc.data();
-      console.log(voterData);
+      
 
       if (voterData.hasVoted) {
         setError("You have already voted");
